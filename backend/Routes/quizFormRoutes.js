6f4d3c05d75f5d5dc4a45e7723b7bd6a4a21ed9a
Mr-mahato <cmhandan@gmail.com>
2025-01-12 18:47:12 +0530

    updated routes

--- backend/Routes/quizFormRoutes.js
+++ backend/Routes/quizFormRoutes.js
@@ -5,10 +5,12 @@ const {
   postFillBlanks,
   postCategory,
   getCategory,
+  getFillTheGaps,
 } = require("../controller/quizController");
 quizRouter.get("/", intro);
 quizRouter.post("/fill-in-the-blanks-question", postFillBlanks);
 quizRouter.post("/create-category-question", postCategory);
 quizRouter.get("/getCategory/:quizId", getCategory);
+quizRouter.get("/getFillGaps/:quizId", getFillTheGaps);
 
 module.exports = quizRouter;
